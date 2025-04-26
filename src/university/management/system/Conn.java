d3c224ff21283892daf125a9d6b8a56333ffcbf3
Anshul bhathija <anshulbhathija966@gmail.com>
2025-04-26 20:28:04 +0530

    finall

--- src/university/management/system/Conn.java
+++ src/university/management/system/Conn.java
@@ -1,1 +1,20 @@
-package university.management.system;import java.sql.Connection;import java.sql.DriverManager;import java.sql.Statement;public class Conn {    Connection connection;    Statement statement;    Conn(){        try {            Class.forName("com.mysql.cj.jdbc.Driver");            connection = DriverManager.getConnection("jdbc:mysql:///universitymanagement","root","AyushVish");            statement = connection.createStatement();        }catch (Exception e){            e.printStackTrace();        }    }}
+package university.management.system;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Conn {
+    Connection connection;
+    Statement statement;
+
+    Conn(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql:///universitymanagement","root","07a17an27g");
+            statement = connection.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
